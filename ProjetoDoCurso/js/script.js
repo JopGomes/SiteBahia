a037b4a122496e3697b26e48bdc4c25d9f934ef4
Joseph Inácio Vieira Oliveira Gomes <94874496+JopGomes@users.noreply.github.com>
2021-12-12 14:14:01 -0300

    CursoHTML

---
+++ ProjetoDoCurso/js/script.js
@@ -0,0 +1,16 @@
+/*var resposta = confirm("Deseja excluir?");
+console.log(resposta); */
+
+const a = 10
+const b = 10
+function somar(a,b){
+    return a+b
+}
+
+console.log(somar(2,5))
+
+document.querySelector("#calcular").addEventListener("click",function(){
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+    alert(parseInt(valorA)+parseInt(valorB))
+});
